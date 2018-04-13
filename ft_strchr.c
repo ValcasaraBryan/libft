9fab7f31ba062ec215dbd8d2b6b340e918a47001
Bryan VALCASARA <brvalcas@e2r8p10.42.fr>
2018-04-13 19:02:20 +0200

    modification

--- ft_strchr.c
+++ ft_strchr.c
@@ -14,7 +14,7 @@
 
 char	*ft_strchr(const char *s, int c)
 {
-	int		i;
+	size_t	i;
 	char	*str;
 	char	d;
 
--- libft.h
+++ libft.h
@@ -31,7 +31,7 @@ typedef struct		s_list
 int					ft_putchar_(int c);
 void				ft_putchar(char c);
 void				ft_putstr(char const *str);
-int					ft_strlen(char *str);
+size_t				ft_strlen(const char *str);
 unsigned int		ft_strlen_(char *str);
 int					ft_atoi(const char *str);
 char				*ft_strcat(char *s1, const char *s2);
@@ -85,5 +85,11 @@ void				ft_putchar_fd(char c, int fd);
 void				ft_putstr_fd(char const *s, int fd);
 void				ft_putendl_fd(char const *s, int fd);
 void				ft_putnbr_fd(int n, int fd);
+t_list				*ft_lstnew(void const *content, size_t content_size);
+//void				ft_lstdelone(t_list **alst, void(*del)(void *, size_t));
+//void				ft_lstdel(t_list **alst, void(*del)(void *, size_t));
+//void				ft_lstadd(t_list **alst, t_list *new);
+//void				ft_lstiter(t_list *lst, void(*f)(t_list *elem));
+//t_list				*ft_lstmap(t_list *lst, t_list *(*f)(t_list *elem));
 
 #endif
