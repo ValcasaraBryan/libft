8f00f107a77cf5014f71280ef8c3bc12ba45b78b
Bryan VALCASARA <brvalcas@e1r6p15.42.fr>
2018-05-01 21:48:13 +0200

    modif strjoin

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,8 +18,8 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	if (!(str = ft_memalloc((size_t)ft_strlen((char *)s1)
-		+ (size_t)ft_strlen((char *)s2) + 1)))
+	if (!(str = ft_memalloc(ft_strlen(s1)
+		+ ft_strlen(s2) + 1)))
 		return (NULL);
 	str = ft_strcat(ft_strcpy(str, s1), s2);
 	return (str);
