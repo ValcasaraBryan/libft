128c801ea6092bfbb04f401d7ecc921806a1e015
ValcasaraBryan <bryanvalcasara@live.fr>
2018-12-09 18:32:11 +0100

    ajout de main_get_next_line.c

---
+++ Programme/main/main_GNL.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: brvalcas <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/27 16:44:31 by brvalcas          #+#    #+#             */
+/*   Updated: 2018/11/27 16:44:50 by brvalcas         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/libft.h"
+#include "../../includes/ft_printf.h"
+#include "../../includes/get_next_line.h"
+
+
+int         main(int argc, char **argv)
+{
+    char    *line;
+    int fd;
+
+    if (argc != 2)
+        return (0);
+    fd = open(argv[1], O_RDWR);   
+    while (get_next_line(fd, &line))
+    {
+        ft_printf("%s\n", line);
+        free(line);
+    }
+    close(fd);
+    return (0);
+}
